3fd9b86f5ede17de6a878f4cf9480efc1c410e32
Joshua Revilla <joshua_revilla@steris.com>
2024-09-10 13:05:51 -0700

    adds relative line number with space n

--- nvim/init.lua
+++ nvim/init.lua
@@ -103,6 +103,10 @@ vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
 -- vim.opt.relativenumber = true
+vim.keymap.set("n", '<leader>n', function()
+  vim.wo.relativenumber = not vim.wo.relativenumber
+end)
+-- set relative line numbers with leader + r + n
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
